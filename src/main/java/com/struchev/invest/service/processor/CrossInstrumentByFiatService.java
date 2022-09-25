a7b190b4a43730bab0d5675ed5ca1ae02ab6fd62
Tatiana Poray <tanyaporay@gmail.com>
2022-09-25 13:10:03 +0300

    2022-09-25T13:09:23.453916+03:00.html

--- src/main/java/com/struchev/invest/service/processor/CrossInstrumentByFiatService.java
+++ src/main/java/com/struchev/invest/service/processor/CrossInstrumentByFiatService.java
@@ -176,7 +176,7 @@ public class CrossInstrumentByFiatService implements ICalculatorService<AInstrum
                     && expectTubeSize > tubeSizePrice * strategy.getTubeAvgAdvanceDown()
                     && expectTubeSize * strategy.getTubeAvgAdvanceDown() < tubeSizePrice
                     && !(tubeSize < expectProfit * strategy.getTubeAvgAdvanceDown()
-                        && getPercentMoveUp(smaSlow) < strategy.getPercentMoveUpError()
+                        //&& getPercentMoveUp(smaSlow) < strategy.getPercentMoveUpError()
                         //&& moveUp < strategy.getPercentMoveUpError()
                         && tubeSizePrice * strategy.getTubeAvgAdvanceDown() > tubeSize)
             ) {
