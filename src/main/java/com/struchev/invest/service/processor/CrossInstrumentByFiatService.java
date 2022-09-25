d32abf3d435320a9db7acb153f7936cc605a7683
Tatiana Poray <tanyaporay@gmail.com>
2022-09-25 17:24:31 +0300

    2022-09-25T17:23:48.694713+03:00.html

--- src/main/java/com/struchev/invest/service/processor/CrossInstrumentByFiatService.java
+++ src/main/java/com/struchev/invest/service/processor/CrossInstrumentByFiatService.java
@@ -175,10 +175,10 @@ public class CrossInstrumentByFiatService implements ICalculatorService<AInstrum
             annotation += " profit (" + moveUp + "): " + tubeSize + ", " + tubeSizePrice + ", " + expectTubeSize + ">" + expectProfit;
             if (/*tubeSize > expectProfit && */expectTubeSize > expectProfit
                     && expectTubeSize > tubeSizePrice * strategy.getTubeAvgAdvanceDown()
-                    //&& expectTubeSize * strategy.getTubeAvgAdvanceDown() < tubeSizePrice
+                    && expectTubeSize * strategy.getTubeAvgAdvanceDown() < tubeSizePrice
                     && !(tubeSize < expectProfit * strategy.getTubeAvgAdvanceDown()
                         //&& getPercentMoveUp(smaSlow) < strategy.getPercentMoveUpError()
-                        && moveUp < 0//strategy.getPercentMoveUpError()
+                        //&& moveUp < 0//strategy.getPercentMoveUpError()
                         && tubeSizePrice * strategy.getTubeAvgAdvanceDown() > tubeSize)
             ) {
                 annotation += "=t";
