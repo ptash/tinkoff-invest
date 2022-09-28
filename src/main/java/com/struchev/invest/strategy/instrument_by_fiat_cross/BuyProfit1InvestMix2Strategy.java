cc2caa7af2f284c000f14524a0214707f701542f
Tatiana Poray <tanyaporay@gmail.com>
2022-09-28 17:31:07 +0300

    2022-09-28T17:20:30.200870+03:00.html

---
+++ src/main/java/com/struchev/invest/strategy/instrument_by_fiat_cross/BuyProfit1InvestMix2Strategy.java
@@ -0,0 +1,37 @@
+package com.struchev.invest.strategy.instrument_by_fiat_cross;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class BuyProfit1InvestMix2Strategy extends BuyProfit1InvestMixStrategy {
+
+    private Map FIGIES = Map.of(
+            "BBG006L8G4H1", 3    // Yandex
+    );
+
+    public Map<String, Integer> getFigies() {
+        return FIGIES;
+    }
+    @Qualifier("buyProfit1InvestPercent02SellWithMaxProfitCrossTube2Strategy")
+    @Autowired
+    private BuyProfit1InvestPercent02SellWithMaxProfitCrossTube2Strategy investStrategy;
+    @Qualifier("buyProfit1InvestPercent02SellWithMaxProfitCrossTubeStrategy")
+    @Autowired
+    private BuyProfit1InvestPercent02SellWithMaxProfitCrossTubeStrategy crisisStrategy;
+
+    public AInstrumentByFiatCrossStrategy getInvestStrategy() {
+        return investStrategy;
+    }
+
+    public AInstrumentByFiatCrossStrategy getCrisisStrategy() {
+        return crisisStrategy;
+    }
+
+    public Double getInvestPercentFromSmaSlowest() { return getDeadLinePercentFromSmaSlowest() / 2; }
+
+    public boolean isEnabled() { return true; }
+}
