bc0c1d7444a5ad7d24b042ec3b0abddd4e68364c
Tatiana Poray <tanyaporay@gmail.com>
2023-01-14 15:14:36 +0300

    FactorialStrategyBBG005DXJS36Dygraphs-2023-01-14T15:14:18.448524+03:00.html

--- src/main/java/com/struchev/invest/strategy/instrument_by_fiat_factorial/AInstrumentByFiatFactorialStrategy.java
+++ src/main/java/com/struchev/invest/strategy/instrument_by_fiat_factorial/AInstrumentByFiatFactorialStrategy.java
@@ -57,8 +57,8 @@ public abstract class AInstrumentByFiatFactorialStrategy extends AStrategy imple
     }
     public List<Integer> getFactorialSizes() { return List.of(1); };
     public Integer getFactorialBestSize() { return 2; };
-    public Integer getFactorialAvgSize() { return 2; };
-    public Integer getFactorialLossSize() { return 2; };
+    public Integer getFactorialAvgSize() { return 1; };
+    public Integer getFactorialLossSize() { return 1; };
 
     public Float getFactorialRatioI() { return -1f; }
     public Float getFactorialRatioValue() { return 0.05f; }
