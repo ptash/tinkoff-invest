bd41728e0ca64684eabc1d5f3ea1c47fe362cc92
Tatiana Poray <tanyaporay@gmail.com>
2023-01-15 20:47:25 +0300

    FactorialStrategyBBG005DXJS36Dygraphs-2023-01-15T20:47:08.120908+03:00.html

--- src/main/java/com/struchev/invest/service/processor/FactorialInstrumentByFiatService.java
+++ src/main/java/com/struchev/invest/service/processor/FactorialInstrumentByFiatService.java
@@ -134,9 +134,9 @@ public class FactorialInstrumentByFiatService implements ICalculatorService<AIns
                 var lossPrevAvg = 0f;
                 var expectProfitPrevAvg = 0f;
                 var expectLossPrevAvg = 0f;
-                if (lowestAvg < candle.getClosingPrice().doubleValue()
+                //if (lowestAvg < candle.getClosingPrice().doubleValue()
                 //        || candleList.get(0).getClosingPrice().doubleValue() > candle.getClosingPrice().doubleValue()
-                ) {
+                //) {
                     for (var i = 0; i < strategy.getBuyCriteria().getTakeLossPercentBetweenLength() - 1; i++) {
                         var factorialPrev = findBestFactorialInPast(strategy, candleListPrev.get(i));
                         var expectProfitPrev = factorialPrev.getExpectProfit();
@@ -164,6 +164,7 @@ public class FactorialInstrumentByFiatService implements ICalculatorService<AIns
                     }
                     //annotation += " expectProfitPrevAvg=" + expectProfitPrevAvg
                     //        + " expectLossPrevAvg=" + expectLossPrevAvg;
+                /*
                     annotation += " lossPrevAvg=" + lossPrevAvg;
                     if (res
                             //&& expectProfitPrevAvg < strategy.getBuyCriteria().getStopLossPercent()
@@ -174,8 +175,8 @@ public class FactorialInstrumentByFiatService implements ICalculatorService<AIns
                         res = true;
                     } else {
                         res = false;
-                    }
-                }
+                    }*/
+                //}
             }
 
             //log.info("FactorialInstrumentByFiatService {} from {} to {} {}", candle.getFigi(), factorial.candleListPast.get(0).getDateTime(), candle.getDateTime(), factorial.candleListFeature.size(), annotation);
