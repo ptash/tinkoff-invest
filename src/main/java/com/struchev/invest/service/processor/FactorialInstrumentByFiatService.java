c0ea3649e079cd2b18988ae958af2939c5d9123e
Tatiana Poray <tanyaporay@gmail.com>
2023-01-15 16:11:15 +0300

    FactorialStrategyBBG005DXJS36Dygraphs-2023-01-15T16:02:32.463839+03:00.html

--- src/main/java/com/struchev/invest/service/processor/FactorialInstrumentByFiatService.java
+++ src/main/java/com/struchev/invest/service/processor/FactorialInstrumentByFiatService.java
@@ -108,7 +108,9 @@ public class FactorialInstrumentByFiatService implements ICalculatorService<AIns
                 var lossPrevAvg = 0f;
                 var expectProfitPrevAvg = 0f;
                 var expectLossPrevAvg = 0f;
-                if (lowestAvg < candle.getClosingPrice().doubleValue()) {
+                if (lowestAvg < candle.getClosingPrice().doubleValue()
+                        || candleList.get(0).getClosingPrice().doubleValue() > candle.getClosingPrice().doubleValue()
+                ) {
                     for (var i = 0; i < strategy.getBuyCriteria().getTakeLossPercentBetweenLength() - 1; i++) {
                         var factorialPrev = findBestFactorialInPast(strategy, candleListPrev.get(i));
                         var expectProfitPrev = factorialPrev.getExpectProfit();
