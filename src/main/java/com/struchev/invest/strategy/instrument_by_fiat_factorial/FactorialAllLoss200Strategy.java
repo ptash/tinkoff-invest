e180ea0eca6e210586f07d43963f8944249900ed
Tatiana Poray <tanyaporay@gmail.com>
2023-01-26 10:06:00 +0300

    0.0.56

--- src/main/java/com/struchev/invest/strategy/instrument_by_fiat_factorial/FactorialAllLoss200Strategy.java
+++ src/main/java/com/struchev/invest/strategy/instrument_by_fiat_factorial/FactorialAllLoss200Strategy.java
@@ -10,6 +10,7 @@ public class FactorialAllLoss200Strategy extends Factorial2Strategy {
 
     private static final Map FIGIES = new HashMap<String, Integer>();
     static {
+        FIGIES.put("BBG00Y91R9T3", 1); // OZON
         FIGIES.put("BBG0029SG1C1", 10); // KZOSP ПАО «КАЗАНЬОРГСИНТЕЗ» - акции привилегированные
         FIGIES.put("BBG002458LF8", 1); // SELG Селигдар
         FIGIES.put("BBG00178PGX3", 1); // VK
