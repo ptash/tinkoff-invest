9c049c0fb177724b91c5aef409c6cae61ee1bb70
Tatiana Poray <tanyaporay@gmail.com>
2023-01-26 09:03:22 +0300

    Factorial2StrategyBBG004730RP0Dygraphs-2023-01-26T08:58:41.984995+03:00.html

---
+++ src/test/java/com/struchev/invest/strategy/instrument_by_fiat_factorial/FactorialAllLoss100Strategy.java
@@ -0,0 +1,19 @@
+package com.struchev.invest.strategy.instrument_by_fiat_factorial;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FactorialAllLoss100Strategy extends Factorial2Strategy {
+
+    public Integer getFactorialHistoryLength() {
+        return this.getFactorialLength() * 100;
+    }
+    public  AInstrumentByFiatFactorialStrategy.BuyCriteria getBuyCriteria() {
+        var buy = super.getBuyCriteria();
+        buy.setIsAllUnderLoss(true);
+        return buy;
+    }
+    public boolean isEnabled() {
+        return false;
+    }
+}
--- src/test/java/com/struchev/invest/strategy/instrument_by_fiat_factorial/FactorialAllLoss200Strategy.java
+++ src/test/java/com/struchev/invest/strategy/instrument_by_fiat_factorial/FactorialAllLoss200Strategy.java
@@ -3,8 +3,12 @@ package com.struchev.invest.strategy.instrument_by_fiat_factorial;
 import org.springframework.stereotype.Component;
 
 @Component
-public class FactorialAllLossStrategy extends Factorial2Strategy {
-    public  AInstrumentByFiatFactorialStrategy.BuyCriteria getBuyCriteria() {
+public class FactorialAllLoss200Strategy extends Factorial2Strategy {
+
+    public Integer getFactorialHistoryLength() {
+        return this.getFactorialLength() * 200;
+    }
+    public  BuyCriteria getBuyCriteria() {
         var buy = super.getBuyCriteria();
         buy.setIsAllUnderLoss(true);
         return buy;
