01fbd4d0a1d35eb26b413eb411ac0c581fe6297b
Tatiana Poray <tanyaporay@gmail.com>
2023-01-30 08:52:40 +0300

    Factorial2TopStrategyBBG0029SG1C1Dygraphs-2023-01-30T08:47:50.489927+03:00.html

--- src/test/java/com/struchev/invest/strategy/instrument_by_fiat_factorial/Factorial2TopStrategy.java
+++ src/test/java/com/struchev/invest/strategy/instrument_by_fiat_factorial/Factorial2TopStrategy.java
@@ -5,6 +5,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class Factorial2TopStrategy extends Factorial2Strategy {
 
+    public Integer getFactorialHistoryLength() {
+        return this.getFactorialLength() * 200;
+    }
+    public Integer getFactorialLengthFuture() { return 8; }
+
     public BuyCriteria getBuyCriteria() {
         var buy = super.getBuyCriteria();
         buy.setIsOverProfit(true);
