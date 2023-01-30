636e3b87b7bc698006ef0bb4036b65fbb4b215cb
Tatiana Poray <tanyaporay@gmail.com>
2023-01-30 19:54:00 +0300

    FactorialStrategyBBG005DXJS36Dygraphs-2023-01-30T19:53:29.426623+03:00.html

---
+++ src/test/java/com/struchev/invest/strategy/instrument_by_fiat_factorial/Factorial2TopMinPriceNullStrategy.java
@@ -0,0 +1,16 @@
+package com.struchev.invest.strategy.instrument_by_fiat_factorial;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Factorial2TopMinPriceNullStrategy extends Factorial2TopStrategy {
+    public BuyCriteria getBuyCriteria() {
+        var buy = super.getBuyCriteria();
+        buy.setProfitPercentFromBuyMinPrice(null);
+        buy.setIsOverProfit(true);
+        return buy;
+    }
+    public boolean isEnabled() {
+        return true;
+    }
+}
