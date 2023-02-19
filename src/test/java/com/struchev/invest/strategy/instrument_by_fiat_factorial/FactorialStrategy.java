7527301b1532c6553225a24005fa89ba3a40998b
Tatiana Poray <tanyaporay@gmail.com>
2023-02-20 00:46:10 +0300

    Factorial3TopAllMinLossStrategyBBG0029SG1C1Dygraphs-2023-02-20T00:39:32.090641+03:00.html

--- src/test/java/com/struchev/invest/strategy/instrument_by_fiat_factorial/FactorialStrategy.java
+++ src/test/java/com/struchev/invest/strategy/instrument_by_fiat_factorial/FactorialStrategy.java
@@ -59,7 +59,7 @@ public class FactorialStrategy extends AInstrumentByFiatFactorialStrategy {
         //FIGIES.put("BBG000RP8V70", 1); // CHMK ЧМК
         //
         //FIGIES.put("BBG00Y91R9T3", 1); // OZON
-        //FIGIES.put("BBG0029SG1C1", 10); // KZOSP ПАО «КАЗАНЬОРГСИНТЕЗ» - акции привилегированные
+        FIGIES.put("BBG0029SG1C1", 10); // KZOSP ПАО «КАЗАНЬОРГСИНТЕЗ» - акции привилегированные
         //FIGIES.put("BBG222222222", 100); // Тинькофф Золото
         FIGIES.put("BBG000NLB2G3", 10); // KROT Красный Октябрь
         //FIGIES.put("BBG005DXJS36", 1); // TCS Group (Tinkoff Bank holder)
