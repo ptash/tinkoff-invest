72358471cefc96df88e879c7239a97074b621202
Tatiana Poray <tanyaporay@gmail.com>
2025-06-18 14:32:36 +0300

    getLimitPriceByTrySell = 1 => 1.8 getLimitPercentByCandle => down 0.6 AlligatorStrategy5MinSLimit2ShortFUTBR0125000Dygraphs-2025-06-18T14_28_16.861560+03_00.html

--- src/test/java/com/struchev/invest/strategy/alligator/AlligatorStrategy5MinSLimit2.java
+++ src/test/java/com/struchev/invest/strategy/alligator/AlligatorStrategy5MinSLimit2.java
@@ -8,4 +8,9 @@ public class AlligatorStrategy5MinSLimit2 extends AlligatorStrategy5MinSLimit {
 
     public Double getMinPercentDeltaByTrySell() { return 0.15; }
     public Double getMaxPercentDeltaByTrySell() { return 0.3; }
+
+    public Double getLimitPercentByCandle() { return .6 * 0.375 / 1.4; }
+    public Double getLimitPriceByTrySell() { return 1.8; }
+
+    public boolean isEnabled() { return true; }
 }
