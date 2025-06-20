c2bea876ede6e311647f7b0eb9516735b10c36fe
Tatiana Poray <tanyaporay@gmail.com>
2025-06-20 13:27:02 +0300

    MaxPercentLimitPrice = 0.1

--- src/test/java/com/struchev/invest/strategy/alligator/AlligatorStrategy5MinSLimitT.java
+++ src/test/java/com/struchev/invest/strategy/alligator/AlligatorStrategy5MinSLimitT.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class AlligatorStrategy5MinSLimitT extends AlligatorStrategy5MinSLimit {
     public Double getMaxPercentStopLossByTrySell() { return 0.5; } // чуть менее рисковано stop loss
-    public Double getMaxPercentLimitPriceByTrySell() { return 0.2; }
+    public Double getMaxPercentLimitPriceByTrySell() { return 0.1; }
     public boolean isMoveStopLossByTrySellByTrend() { return true; }
     public boolean isEnabled() { return true; }
 }
