e75d749ccac5f3723cca19510980394b6083b549
Tatiana Poray <tanyaporay@gmail.com>
2025-12-09 09:32:54 +0300

    0.0.370

---
+++ src/main/java/com/struchev/invest/strategy/alligator/AlligatorStrategy5MinRev.java
@@ -0,0 +1,17 @@
+package com.struchev.invest.strategy.alligator;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AlligatorStrategy5MinRev extends AlligatorStrategy5Min {
+    public boolean isReverse() { return true; }
+    public boolean isSkipBySmaFarGreenBlue() { return false; }
+
+    public Double getMaxGreenPercent() { return 3.0; }
+
+    public Float getBuyMinProfitPercent() { return .15f; }
+
+    public SellLimitCriteria getSellLimitCriteriaOrig() {
+        return SellLimitCriteria.builder().exitProfitPercent(2.0f).build();
+    }
+}
---
+++ src/main/java/com/struchev/invest/strategy/alligator/AlligatorStrategy5MinRev2.java
@@ -0,0 +1,37 @@
+package com.struchev.invest.strategy.alligator;
+
+import com.struchev.invest.expression.Date;
+import org.springframework.stereotype.Component;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class AlligatorStrategy5MinRev2 extends AlligatorStrategy5MinRev {
+
+    public boolean isBuyMaxOnlySmaUp() { return true; }
+    public Integer getReverseMaxLength() { return 15; }
+    public Integer getReverseUpMinLength() { return 10; }
+    public Integer getTrendUpLength() { return 5; }
+    public boolean isStopLossForce() { return true; }
+    public boolean isStopLossForcePrev() { return true; }
+    //public boolean isSmaNearGreenBlueIsTrendDown() { return true; }
+    public Double getReverseStopLossK() { return 1.0; }
+
+    public OffsetDateTime getDayTimeEndTrading(OffsetDateTime dateTime) {
+        var dateInZone = Date.getDateTimeInZone(dateTime);;
+        if (dateInZone.getDayOfWeek().getValue() < 6) {
+            return OffsetDateTime.parse("2000-01-01T23:30:00+03:00", DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+        } else {
+            return OffsetDateTime.parse("2000-01-01T18:35:00+03:00", DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+        }
+    }
+    public OffsetDateTime getDayTimeEndBuy(OffsetDateTime dateTime) {
+        var dateInZone = Date.getDateTimeInZone(dateTime);;
+        if (dateInZone.getDayOfWeek().getValue() < 6) {
+            return OffsetDateTime.parse("2000-01-01T22:05:00+03:00", DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+        } else {
+            return OffsetDateTime.parse("2000-01-01T17:10:00+03:00", DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+        }
+    }
+}
