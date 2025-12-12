a269d7f8e354ca4ac8ce1bbf26559e1ca512591f
Tatiana Poray <tanyaporay@gmail.com>
2025-12-12 21:53:48 +0300

    getLimitPriceDownStepLength = 15

--- src/test/java/com/struchev/invest/strategy/alligator/AlligatorStrategy5MinRev4.java
+++ src/test/java/com/struchev/invest/strategy/alligator/AlligatorStrategy5MinRev4.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 public class AlligatorStrategy5MinRev4 extends AlligatorStrategy5MinRev3 {
     public Boolean isUpLimitPriceToMinProfitPercent() {return true; }
     public Double getDownFromPriceWantedK() { return 1.; }
-    public Integer getLimitPriceDownStepLength() { return 10; }
+    public Integer getLimitPriceDownStepLength() { return 15; }
     public Integer getMinLowestPriceUnderAnyLength() { return 2; }
     public boolean isEnabled() { return true; }
 }
