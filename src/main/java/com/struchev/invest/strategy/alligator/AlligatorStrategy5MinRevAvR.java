8bb15e6efefc3253600b22d6b3b1032850bc5437
Tatiana Poray <tanyaporay@gmail.com>
2026-01-04 01:58:21 +0300

    fix

--- src/main/java/com/struchev/invest/strategy/alligator/AlligatorStrategy5MinRevAvR.java
+++ src/main/java/com/struchev/invest/strategy/alligator/AlligatorStrategy5MinRevAvR.java
@@ -12,5 +12,6 @@ public class AlligatorStrategy5MinRevAvR extends AlligatorStrategy5MinRevAv {
 
     public boolean isStopLossForce() { return false; }
     public boolean isStopLossByLimit() { return true; }
+    public Integer getLimitPriceMaxK() { return 4; }
     public Integer getLimitPriceDownStepLength() { return 0; }
 }
