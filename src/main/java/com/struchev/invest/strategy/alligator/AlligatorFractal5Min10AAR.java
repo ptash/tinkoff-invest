fddcf2f34173ef02c9b4a61b5d959db9e1f76d20
Tatiana Poray <tanyaporay@gmail.com>
2026-01-17 17:47:32 +0300

    AAR

---
+++ src/main/java/com/struchev/invest/strategy/alligator/AlligatorFractal5Min10AAR.java
@@ -0,0 +1,8 @@
+package com.struchev.invest.strategy.alligator;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AlligatorFractal5Min10AAR extends AlligatorFractal5Min10AA {
+    public boolean isRev() { return true; }
+}
