a92386c5821315e607730e1541964031f9651dbe
Tatiana Poray <tanyaporay@gmail.com>
2026-01-24 22:51:12 +0300

    true = isBuyMaxOnlySmaDown

--- src/test/java/com/struchev/invest/strategy/alligator/AlligatorFractal5Min10AARN3.java
+++ src/test/java/com/struchev/invest/strategy/alligator/AlligatorFractal5Min10AARN3.java
@@ -7,7 +7,7 @@ public class AlligatorFractal5Min10AARN3 extends AlligatorFractal5Min10AAN3 {
     public boolean isRev() { return true; }
     public boolean isStopLossOnlyByLimit() { return true; }
     public boolean isBuyMaxOnlySmaUp() { return false; }
-    public boolean isBuyMaxOnlySmaDown() { return false; }
+    public boolean isBuyMaxOnlySmaDown() { return true; }
     public boolean isPriceWantedAv() { return false; }
     public boolean isEnabled() { return true; }
 }
