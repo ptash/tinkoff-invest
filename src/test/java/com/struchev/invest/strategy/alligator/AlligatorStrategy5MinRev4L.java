4c11849ba526d7c2b43e16e467037b82561728e2
Tatiana Poray <tanyaporay@gmail.com>
2026-01-31 18:04:53 +0300

    LimitPriceDownStepLength = 0

--- src/test/java/com/struchev/invest/strategy/alligator/AlligatorStrategy5MinRev4L.java
+++ src/test/java/com/struchev/invest/strategy/alligator/AlligatorStrategy5MinRev4L.java
@@ -8,5 +8,6 @@ public class AlligatorStrategy5MinRev4L extends AlligatorStrategy5MinRev4 {
     public Integer getReverseLongMinAvLength() { return 5; }
     public Double getReverseLongMinAvAdK() { return 0.6; }
     public Integer getReverseSellLongMinMinLength() { return 3; }
+    public Integer getLimitPriceDownStepLength() { return 0; }
     public boolean isEnabled() { return true; }
 }
