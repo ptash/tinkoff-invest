ed380391e0c0ea803e59831d77c11fe24e30d366
Tatiana Poray <tanyaporay@gmail.com>
2026-01-31 15:36:37 +0300

    Rev4L LongMinLength = 40 & LongMinAvAdK = null

--- src/test/java/com/struchev/invest/strategy/alligator/AlligatorStrategy5MinRev4L.java
+++ src/test/java/com/struchev/invest/strategy/alligator/AlligatorStrategy5MinRev4L.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AlligatorStrategy5MinRev4L extends AlligatorStrategy5MinRev4 {
-    public Integer getReverseLongMinLength() { return 35; }
+    public Integer getReverseLongMinLength() { return 40; }
     public Integer getReverseLongMinAvLength() { return 5; }
+    public Double getReverseLongMinAvAdK() { return null; }
     public boolean isEnabled() { return true; }
 }
