f8f4b8347c1c58072b4ecead2eddbb9f952a9662
Tatiana Poray <tanyaporay@gmail.com>
2026-02-03 16:41:18 +0300

    0 = getReverseSellLongMinMinLength

--- src/main/java/com/struchev/invest/strategy/alligator/AlligatorStrategy5MinRev5L.java
+++ src/main/java/com/struchev/invest/strategy/alligator/AlligatorStrategy5MinRev5L.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 public class AlligatorStrategy5MinRev5L extends AlligatorStrategy5MinRev5 {
     public Integer getReverseLongMinLength() { return 35; }
     public Integer getReverseLongMinAvLength() { return 5; }
-    public Integer getReverseSellLongMinMinLength() { return 3; }
+    //public Integer getReverseSellLongMinMinLength() { return 3; }
     public Integer getReverseBuyLongMinMinLength() { return 3; }
     public Integer getReverseBuyMinMinLength() { return 3; }
     public Integer getLimitPriceDownStepLength() { return 0; }
--- src/test/java/com/struchev/invest/strategy/alligator/AlligatorStrategy5MinRev4L.java
+++ src/test/java/com/struchev/invest/strategy/alligator/AlligatorStrategy5MinRev4L.java
@@ -11,7 +11,7 @@ public class AlligatorStrategy5MinRev4L extends AlligatorStrategy5MinRev4 {
     public Integer getReverseLongMinLength() { return 40; }
     public Integer getReverseLongMinAvLength() { return 5; }
     public Double getReverseLongMinAvAdK() { return 0.6; }
-    public Integer getReverseSellLongMinMinLength() { return 3; }
+    //public Integer getReverseSellLongMinMinLength() { return 3; }
     public Integer getReverseBuyLongMinMinLength() { return 3; }
     public Integer getReverseBuyMinMinLength() { return 3; }
     public Integer getLimitPriceDownStepLength() { return 0; }
