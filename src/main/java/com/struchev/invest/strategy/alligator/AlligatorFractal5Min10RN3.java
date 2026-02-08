b2132e4729d533ed838b8cb835e4b2996277fe7a
Tatiana Poray <tanyaporay@gmail.com>
2026-02-09 02:02:39 +0300

    AlligatorFractal5Min10RN3

---
+++ src/main/java/com/struchev/invest/strategy/alligator/AlligatorFractal5Min10RN3.java
@@ -0,0 +1,8 @@
+package com.struchev.invest.strategy.alligator;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AlligatorFractal5Min10RN3 extends AlligatorFractal5Min10R {
+    public Integer getFractalAverageNumber() { return 3; }
+}
